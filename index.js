0ced4e898019fc5e7df253b464b4b48bc378bd45
vlodkow <vlodkow@gmail.com>
2022-08-10 20:19:45 +0300

    Update listeners & init

--- index.js
+++ index.js
@@ -47,17 +47,20 @@ if (window.ethereum) {
     await provider.send("eth_requestAccounts", []);
     const signer = provider.getSigner();
     ct.web3.userAddress = await signer.getAddress();
-
     console.log("Account:", ct.web3.userAddress);
 
     try {
-      const contractABI = [/*%contractABI%*/][0];
-      ct.web3.contract = new ethers.Contract(
-        ct.web3.contractAddress,
-        contractABI.abi ? contractABI.abi.abi : contractABI.abi,
-        signer
-      );
-      ct.web3.isConnected = true;
+      isCorrectNetwork().then(isCorrect => {
+        if (isCorrect) {
+          const contractABI = [/*%contractABI%*/][0];
+          ct.web3.contract = new ethers.Contract(
+            ct.web3.contractAddress,
+            contractABI.abi ? contractABI.abi : contractABI,
+            signer
+          );
+          ct.web3.isConnected = true;
+        }
+      });
     } catch (e) {
       alert("Error on contract initialisation, please check Contract ABI");
     }
@@ -90,166 +93,181 @@ if (window.ethereum) {
     }
   }
 
-  const initNetwork = async () => {
-    isCorrectNetwork().then(isCorrect => {
-      if (!isCorrect) {
-        // Add switch network listener
-        document.querySelector(".web3-btn").addEventListener("click", switchNetwork);
+  const initAlchemy = () => {
+    const callAlchemyAPI = async (method, params) => {
+      const apiKey = "/*%alchemyKey%*/";
+      const baseURL = window.defaultChainSettings["/*%chain%*/"].alchemyURL;
 
-        // Show wrong network error message
-        const alertMessage = document.getElementById('wrong-network-alert');
-        alertMessage.classList.add('visible');
-      }
-    });
+      if (baseURL.length) {
+        let fetchURL = `${baseURL}/${apiKey}/${method}/`;
+        if (Object.keys(params).length) {
+          const searchParams = Object.keys(params).map(key => {
+            if (Array.isArray(params[key])) {
+              return params[key].map(item => key + '[]=' + item).join('&');
+            } else {
+              return key + '=' + params[key];
+            }
+          }).join('&');
+          fetchURL += `?${searchParams}`;
+        }
 
-    // Reload game on switch chain
-    window.ethereum.on("chainChanged", () => {
-      // window.location.reload();
-      ct.web3.connect();
-    });
-
-    // Account change - connect and load new signer
-    window.ethereum.on("accountsChanged", (account) => {
-      if (account.length) {
-        // account switched
-        ct.web3.connect();
+        return await fetch(fetchURL, {
+          method: 'GET',
+          redirect: 'follow'
+        }).then(response => response.json());
       } else {
-        // metamask disconnected
-        window.location.reload();
+        alert('Chain not supported');
+      }
+    }
+
+    /**
+     * Gets all NFTs currently owned by a given address.
+     * @param owner string (required) - Address for NFT owner.
+     * @param pageKey string - UUID for pagination.
+     * @param contractAddresses array of strings - Array of contract addresses to filter the responses with.
+     * @param withMetadata boolean - if true query will include metadata for each returned token.
+     * @param filters array of strings - Array of filters (as ENUMS) that will be applied to the query.
+     * @link https://docs.alchemy.com/reference/getnfts
+     * @return Promise
+     */
+    ct.web3.nft.getNFTs = (params) => {
+      return new Promise((resolve, reject) => {
+        callAlchemyAPI('getNFTs', params).then(result => {
+          resolve(result);
+        }).catch(err => reject(err));
+      });
+    };
+
+    /**
+     * Gets the metadata associated with a given NFT.
+     * @param contractAddress string (required) - Address of NFT contract.
+     * @param tokenId string (required) - Integer or Hexadecimal - Id for NFT.
+     * @param tokenType string - 'ERC721' or 'ERC1155'.
+     * @link https://docs.alchemy.com/reference/getnftmetadata
+     * @return Promise
+     */
+    ct.web3.nft.getNFTMetadata = (params) => {
+      return new Promise((resolve, reject) => {
+        callAlchemyAPI('getNFTMetadata', params).then(result => {
+          resolve(result);
+        }).catch(err => reject(err));
+      });
+    };
+
+    /**
+     * Queries NFT high-level collection/contract level information.
+     * @param contractAddress string (required) - Address of NFT contract.
+     * @link https://docs.alchemy.com/reference/getcontractmetadata
+     * @return Promise
+     */
+    ct.web3.nft.getContractMetadata = (params) => {
+      return new Promise((resolve, reject) => {
+        callAlchemyAPI('getContractMetadata', params).then(result => {
+          resolve(result);
+        }).catch(err => reject(err));
+      });
+    };
+
+    /**
+     * Gets all NFTs for a given NFT contract.
+     * @param contractAddress string (required) - Address of NFT contract.
+     * @param withMetadata boolean - if true returns NFT metadata, otherwise will only return tokenIds.
+     * @param startToken string - An offset used for pagination.
+     * @param limit integer - Sets the total number of NFTs returned in the response. Defaults to 100.
+     * @link https://docs.alchemy.com/reference/getnftsforcollection
+     * @return Promise
+     */
+    ct.web3.nft.getNFTsForCollection = (params) => {
+      return new Promise((resolve, reject) => {
+        callAlchemyAPI('getNFTsForCollection', params).then(result => {
+          resolve(result);
+        }).catch(err => reject(err));
+      });
+    };
+
+    /**
+     * Get the owner(s) for a token.
+     * @param contractAddress string (required) - Address of NFT contract.
+     * @param tokenId string (required) - The ID of the token
+     * @link https://docs.alchemy.com/reference/getownersfortoken
+     * @return Promise
+     */
+    ct.web3.nft.getOwnersForToken = (params) => {
+      return new Promise((resolve, reject) => {
+        callAlchemyAPI('getOwnersForToken', params).then(result => {
+          resolve(result);
+        }).catch(err => reject(err));
+      });
+    };
+
+    /**
+     * Gets all owners for a given NFT contract.
+     * @param contractAddress string (required) - Address of NFT collection.
+     * @param withTokenBalances boolean - if true the query will include the token balances per token id for each owner.
+     * @link https://docs.alchemy.com/reference/getownersforcollection
+     * @return Promise
+     */
+    ct.web3.nft.getOwnersForCollection = (params) => {
+      return new Promise((resolve, reject) => {
+        callAlchemyAPI('getOwnersForCollection', params).then(result => {
+          resolve(result);
+        }).catch(err => reject(err));
+      });
+    };
+  }
+
+  // Reload game on switch chain
+  const chainChangeEvent = () => {
+    initModule();
+  };
+
+  // Account change - connect and load new signer
+  const accountChangeEvent = (account) => {
+    if (account.length) {
+      initModule();
+    } else {
+      // metamask disconnected
+      window.location.reload();
+    }
+  };
+
+  const initModule = () => {
+    const web3SwitchNetwork = document.querySelector(".web3-btn");
+
+    // Cleanup listeners
+    window.ethereum.removeListener("chainChanged", chainChangeEvent);
+    window.ethereum.removeListener("accountsChanged", accountChangeEvent);
+    web3SwitchNetwork.removeEventListener("click", switchNetwork);
+
+    // Add listeners
+    window.ethereum.on("chainChanged", chainChangeEvent);
+    window.ethereum.on("accountsChanged", accountChangeEvent);
+    web3SwitchNetwork.addEventListener("click", switchNetwork);
+
+    isCorrectNetwork().then(isCorrect => {
+      const alertMessage = document.getElementById('wrong-network-alert');
+      if (!isCorrect) {
+        // Show wrong network error message
+        alertMessage.classList.add('visible');
+      } else {
+        // Hide wrong network error message
+        alertMessage.classList.remove('visible');
+
+        // Connect Metamask on init
+        if (ct.web3.connectOnInit) {
+          ct.web3.connect();
+        }
+
+        // Alchemy NFT API methods
+        if ("/*%alchemyKey%*/") {
+          initAlchemy();
+        }
       }
     });
   }
 
-  const callAlchemyAPI = async (method, params) => {
-    const apiKey = "/*%alchemyKey%*/";
-    const baseURL = window.defaultChainSettings["/*%chain%*/"].alchemyURL;
-
-    if (baseURL.length) {
-      let fetchURL = `${baseURL}/${apiKey}/${method}/`;
-      if (Object.keys(params).length) {
-        const searchParams = Object.keys(params).map(key => {
-          if (Array.isArray(params[key])) {
-            return params[key].map(item => key + '[]=' + item).join('&');
-          } else {
-            return key + '=' + params[key];
-          }
-        }).join('&');
-        fetchURL += `?${searchParams}`;
-      }
-
-      return await fetch(fetchURL, {
-        method: 'GET',
-        redirect: 'follow'
-      }).then(response => response.json());
-    } else {
-      alert('Chain not supported');
-    }
-  }
-
-  initNetwork().then(() => {
-    // Connect Metamask
-    if (ct.web3.connectOnInit) {
-      ct.web3.connect();
-    }
-
-    // Add Alchemy NFT API methods
-    if ("/*%alchemyKey%*/") {
-      /**
-       * Gets all NFTs currently owned by a given address.
-       * @param owner string (required) - Address for NFT owner.
-       * @param pageKey string - UUID for pagination.
-       * @param contractAddresses array of strings - Array of contract addresses to filter the responses with.
-       * @param withMetadata boolean - if true query will include metadata for each returned token.
-       * @param filters array of strings - Array of filters (as ENUMS) that will be applied to the query.
-       * @link https://docs.alchemy.com/reference/getnfts
-       * @return Promise
-       */
-      ct.web3.nft.getNFTs = (params) => {
-        return new Promise((resolve, reject) => {
-          callAlchemyAPI('getNFTs', params).then(result => {
-            resolve(result);
-          }).catch(err => reject(err));
-        });
-      };
-
-      /**
-       * Gets the metadata associated with a given NFT.
-       * @param contractAddress string (required) - Address of NFT contract.
-       * @param tokenId string (required) - Integer or Hexadecimal - Id for NFT.
-       * @param tokenType string - 'ERC721' or 'ERC1155'.
-       * @link https://docs.alchemy.com/reference/getnftmetadata
-       * @return Promise
-       */
-      ct.web3.nft.getNFTMetadata = (params) => {
-        return new Promise((resolve, reject) => {
-          callAlchemyAPI('getNFTMetadata', params).then(result => {
-            resolve(result);
-          }).catch(err => reject(err));
-        });
-      };
-
-      /**
-       * Queries NFT high-level collection/contract level information.
-       * @param contractAddress string (required) - Address of NFT contract.
-       * @link https://docs.alchemy.com/reference/getcontractmetadata
-       * @return Promise
-       */
-      ct.web3.nft.getContractMetadata = (params) => {
-        return new Promise((resolve, reject) => {
-          callAlchemyAPI('getContractMetadata', params).then(result => {
-            resolve(result);
-          }).catch(err => reject(err));
-        });
-      };
-
-      /**
-       * Gets all NFTs for a given NFT contract.
-       * @param contractAddress string (required) - Address of NFT contract.
-       * @param withMetadata boolean - if true returns NFT metadata, otherwise will only return tokenIds.
-       * @param startToken string - An offset used for pagination.
-       * @param limit integer - Sets the total number of NFTs returned in the response. Defaults to 100.
-       * @link https://docs.alchemy.com/reference/getnftsforcollection
-       * @return Promise
-       */
-      ct.web3.nft.getNFTsForCollection = (params) => {
-        return new Promise((resolve, reject) => {
-          callAlchemyAPI('getNFTsForCollection', params).then(result => {
-            resolve(result);
-          }).catch(err => reject(err));
-        });
-      };
-
-      /**
-       * Get the owner(s) for a token.
-       * @param contractAddress string (required) - Address of NFT contract.
-       * @param tokenId string (required) - The ID of the token
-       * @link https://docs.alchemy.com/reference/getownersfortoken
-       * @return Promise
-       */
-      ct.web3.nft.getOwnersForToken = (params) => {
-        return new Promise((resolve, reject) => {
-          callAlchemyAPI('getOwnersForToken', params).then(result => {
-            resolve(result);
-          }).catch(err => reject(err));
-        });
-      };
-
-      /**
-       * Gets all owners for a given NFT contract.
-       * @param contractAddress string (required) - Address of NFT collection.
-       * @param withTokenBalances boolean - if true the query will include the token balances per token id for each owner.
-       * @link https://docs.alchemy.com/reference/getownersforcollection
-       * @return Promise
-       */
-      ct.web3.nft.getOwnersForCollection = (params) => {
-        return new Promise((resolve, reject) => {
-          callAlchemyAPI('getOwnersForCollection', params).then(result => {
-            resolve(result);
-          }).catch(err => reject(err));
-        });
-      };
-    }
-  });
+  // Init module
+  initModule();
 
 } else {
   const alertMessage = document.getElementById('no-metamask-alert');
